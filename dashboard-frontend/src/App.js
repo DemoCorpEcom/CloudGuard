5413c8febda0faa0ee4a5febd0bbc7641472747b
Joshua Mathew <umjoshua@gmail.com>
2023-03-08 21:29:44 +0530

    frontend

--- dashboard-frontend/src/App.js
+++ dashboard-frontend/src/App.js
@@ -1,5 +1,5 @@
 import Appbar from "./components/Appbar/Appbar";
-import Sidebar from "./components/sidebar/Sidebar";
+import Sidebar from "./components/topbar/topbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScanStatus from "./components/scanStatus/scanStatus";
 import Results from "./components/results/results";
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter>
       <div className="h-full">
         <Appbar />
-        <div className="flex flex-row h-full">
+        <div className="h-full w-full">
           <Sidebar />
           <Routes>
             <Route exact path="/" element={<ScanStatus />}></Route>
--- dashboard-frontend/src/components/results/results.js
+++ dashboard-frontend/src/components/results/results.js
@@ -1,9 +1,44 @@
 import React from "react"
+import './results.css';
 
 const Results = () => {
+
+    const Result = () => {
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th className="second">Commit: </th>
+                    </tr>
+                    <tr className="head">
+                        <th className="second">Vulnerability</th>
+                        <th className="third">Affected URL</th>
+                        <th className="fourth">Severity</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td className="second">Error based SQL Injection</td>
+                        <td className="third">www.google.com</td>
+                        <td className="fourth">High</td>
+                    </tr>
+                    <tr>
+                        <td className="second">XSS</td>
+                        <td className="third">www.google.com</td>
+                        <td className="fourth">High</td>
+                    </tr>
+
+                </tbody>
+
+            </table>
+        )
+    }
+
     return (
-        <div>
-            <h1>Results</h1>
+        <div className="w-full">
+            <Result />
+            <Result />
         </div>
     )
 }
