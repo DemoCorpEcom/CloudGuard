418ee2ef6f5e0e81c0e0766ce463df3c244fa803
Joshua Mathew <umjoshua@gmail.com>
2023-03-11 11:34:31 +0530

    appbar updated

--- dashboard-frontend/src/components/Appbar/Appbar.js
+++ dashboard-frontend/src/components/Appbar/Appbar.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import Scan from '../../assets/icons/scan.svg'
+import Result from '../../assets/icons/result.svg'
+
 
 const Appbar = () => {
     return (
-        <div className='p-4'>
-            <p>Admin Dashboard </p>
-        </div>
+        <div className='p-2 flex flex-row items-center border-y-2 shadow-xl bg-white'>
+            <div className="font-pacifico text-2xl text-blue-800">Admin Dashboard </div>
+            <div className='ml-4 text-xl flex flex-row'>
+                <NavLink to="/" className="flex flex-row items-center p-2 mx-2 rounded">
+                    <img src={Scan} alt="scan" className="p-2" />
+                    Scan Status
+                </NavLink>
+                <NavLink to="/results" className="flex flex-row p-2 mx-2 rounded items-center">
+                    <img src={Result} alt="result" className="p-2" />
+                    Results
+                </NavLink>
+            </div>
+        </div >
     )
 }
 
