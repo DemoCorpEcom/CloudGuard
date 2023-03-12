6de2d4393116e67f08f3ec595a0ee130abf3554c
Joshua Mathew <umjoshua@gmail.com>
2023-03-12 15:13:40 +0530

    view links popup

---
+++ dashboard-frontend/src/components/results/popup.js
@@ -0,0 +1,31 @@
+import Popup from 'reactjs-popup';
+import './Popup.css'
+
+const ViewLinks = ({ data }) => {
+    console.log(data)
+    return (
+        <Popup trigger={<button>View Links</button>} position="right center">
+            {
+                close => (<div>
+                    <div className="popup overflow-scroll">
+                        <div className="popup_header ">
+                            <h1 className="popup_title">Affected URLs</h1>
+                            <h1 className="popupclose_btn" onClick={close}>&times;</h1>
+                        </div>
+                        <div className="details">
+                            {
+                                data.map((item) => {
+                                    return (
+                                        <p><a href={item.affectedUrl}>{item.affectedUrl}</a></p>
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
+                </div >)
+            }
+        </Popup >
+    )
+}
+
+export default ViewLinks
--- dashboard-frontend/src/components/results/results.js
+++ dashboard-frontend/src/components/results/results.js
@@ -1,86 +1,31 @@
 import React, { useState, useEffect } from "react"
 import './results.css';
 import axios from 'axios';
+import ViewLinks from "./popup";
 
 const Results = () => {
 
-    const [data, setData] = useState({
-        "54467b00418b3e7e4271710f119fa5c7fc1ad866": {
-            "1": [
-                {
-                    "_id": "640c80bebd02cd923cd3ca6d",
-                    "commitId": "54467b00418b3e7e4271710f119fa5c7fc1ad866",
-                    "vulnerability": "Error based SQL injection",
-                    "vulId": 1,
-                    "affectedUrl": "http://192.168.1.7/dcp/product.php?id=1",
-                    "severity": "high",
-                    "__v": 0
-                },
-                {
-                    "_id": "640c80bebd02cd923cd3ca6f",
-                    "commitId": "54467b00418b3e7e4271710f119fa5c7fc1ad866",
-                    "vulnerability": "Error based SQL injection",
-                    "vulId": 1,
-                    "affectedUrl": "http://192.168.1.7/dcp//product.php?id=2",
-                    "severity": "high",
-                    "__v": 0
-                },
-                {
-                    "_id": "640c80bebd02cd923cd3ca71",
-                    "commitId": "54467b00418b3e7e4271710f119fa5c7fc1ad866",
-                    "vulnerability": "Error based SQL injection",
-                    "vulId": 1,
-                    "affectedUrl": "http://192.168.1.7/dcp/product.php?id=3",
-                    "severity": "high",
-                    "__v": 0
-                }
-            ],
-            "2": [
-                {
-                    "_id": "640c80d5efaa9cc432f364c4",
-                    "commitId": "54467b00418b3e7e4271710f119fa5c7fc1ad866",
-                    "vulnerability": "XSS",
-                    "vulId": 2,
-                    "affectedUrl": "http://192.168.1.7/dcp//product.php?id=2",
-                    "severity": "high",
-                    "__v": 0
-                }
-            ]
-        },
-        "54467b00418b3e7e4271710f119fa5c7fc1ad867": {
-            "1": [
-                {
-                    "_id": "640cba35c2f31ba11a7f4d5d",
-                    "commitId": "54467b00418b3e7e4271710f119fa5c7fc1ad867",
-                    "vulnerability": "Error based SQL injection",
-                    "vulId": 1,
-                    "affectedUrl": "http://192.168.1.7/dcp/product.php?id=3",
-                    "severity": "high",
-                    "__v": 0
-                }
-            ]
-        }
-    });
+    const high = "fourth text-white bg-red-500 text-center rounded"
+    const medium = "fourth text-white bg-orange-500 text-center rounded"
+    const low = "fourth text-white bg-yellow-500 text-center rounded"
 
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         const results = await axios.get("http://localhost:5000/api/results");
-    //         setData(results.data);
-    //     };
-    //     fetchData();
-    // }, []);
+    const [data, setData] = useState({});
 
-    // useEffect(() => {
-    //     console.log(data);
-    // }, [data])
+    useEffect(() => {
+        const fetchData = async () => {
+            const results = await axios.get("http://localhost:5000/api/results");
+            setData(results.data);
+        };
+        fetchData();
+    }, []);
 
-    const viewPopup = ()=>{
-        window.alert('hi');
-    }
+    useEffect(() => {
+        console.log(data);
+    }, [data])
 
     return (
-        <div className="flex items-center justify-center mainDiv">
-            <div className="w-[65%] flex flex-row">
+        <div className="flex items-center justify-center mainDiv h-full">
+            <div className="w-[65%] flex flex-row h-full">
                 <div className="flex-1">
                     {data &&
                         Object.keys(data).map((commitId) => {
@@ -101,15 +46,14 @@ const Results = () => {
                                             Object.keys(data[commitId]).map((item) => {
 
                                                 const it = data[commitId][item];
-                                                console.log(data[commitId][item])
                                                 const severity = it[0].severity;
 
                                                 return (
                                                     <tr>
                                                         <td className="second text-red-600">{it[0].vulnerability}</td>
-                                                        <td className="third"><button onClick={viewPopup}>View</button></td>
+                                                        <td className="third"><ViewLinks data={it} /></td>
                                                         <td className={
-                                                            severity === "high" ? "fourth text-white bg-red-500 text-center rounded" : null}
+                                                            severity === "high" ? high : severity === "medium" ? medium : low}
                                                         >
                                                             {severity}
                                                         </td>
