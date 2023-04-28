8fc2bf1c2f9c7dde6c61df68b10df1e8aa789109
Joshua Mathew <umjoshua@gmail.com>
2023-04-28 22:39:32 +0530

    frontend update

--- dashboard-frontend/src/components/results/popup.js
+++ dashboard-frontend/src/components/results/popup.js
@@ -2,14 +2,13 @@ import Popup from 'reactjs-popup';
 import './Popup.css'
 
 const ViewLinks = ({ data }) => {
-    console.log(data)
     return (
         <Popup trigger={<button>View Links</button>} position="right center">
             {
                 close => (<div>
                     <div className="popup overflow-scroll">
                         <div className="popup_header ">
-                            <h1 className="popup_title">Affected URLs</h1>
+                            <h1 className="popup_title">Affected URLs - {data[0].vulnerability}</h1>
                             <h1 className="popupclose_btn" onClick={close}>&times;</h1>
                         </div>
                         <div className="details">
